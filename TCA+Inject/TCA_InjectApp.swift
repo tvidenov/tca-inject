4e71d87927bb04fee5d389779a49ed113e2f21ff
Tihomir Videnov <tihomirvidenov@gmail.com>
2024-01-30 17:02:03 +0200

    Initial Commit

---
+++ TCA+Inject/TCA_InjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_InjectApp.swift
+//  TCA+Inject
+//
+//  Created by Tihomir Videnov on 30.01.24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_InjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
